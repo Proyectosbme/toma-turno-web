56198edb25515b953dffdb477451c12c5dc58249
ivan.marroquin <bryan.marroquin@coop1.com.sv>
2026-05-22 16:29:44 -0600

    Actualizar usuario en keycloak

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     apiUrl: '/api',
-    wsUrl: 'ws://192.168.10.111:8085/turnos',
+    wsUrl: 'ws://localhost:8085/turnos',
     keycloak: {
         url:      'https://sso-tomaturnos.coop1.com.sv',
         realm:    'servicios-tomaturno',
